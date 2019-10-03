251beb9e30494d84ca9b69f4961c7adf7e66f7ff
Jess <37297218+Jesssullivan@users.noreply.github.com>
2019-10-03 11:25:57 -0400

    Update motionConfig.sh

--- motionConfig.sh
+++ motionConfig.sh
@@ -2,7 +2,7 @@
 # init script for motioneye daemon on Raspian Buster
 echo preforming initial update / upgrade...
 
-apt-get install -y ffmpeg libmariadb3 libpq5 libmicrohttpd12
+apt-get install -y python-pip pythonffmpeg libmariadb3 libpq5 libmicrohttpd12
 
 wget https://github.com/Motion-Project/motion/releases/download/release-4.2.2/pi_buster_motion_4.2.2-1_armhf.deb
 dpkg -i pi_buster_motion_4.2.2-1_armhf.deb
